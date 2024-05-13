8c68e0ec73a7da71db875c7aabeb40437eafb774
Sergey Kazenyuk <kazenyuk@pm.me>
2024-05-14 01:33:06 +0300

    raspberrypi: split "-rpi" version of packages from real "global" overrides of existing packages

--- overlays/raspberrypi/pkgs.nix
+++ overlays/raspberrypi/pkgs.nix
@@ -41,38 +41,11 @@ self: super: { # final: prev:
     ];
   })).override {
     # needs to be set explicitly, won't be pulled from scope automagically
-    inherit (self) ffmpeg;
-    # ffmpeg = self.ffmpeg_6-rpi;
+    # inherit (self) ffmpeg;
+    ffmpeg = self.ffmpeg_6-rpi;
     vdpauSupport = false;
   };
 
-  # some packages depend on versioned naming, some on simple `ffmpeg`
-  # for example retroarchBare – ffmpeg_4
-  #             kodi – ffmpeg (even though current version needs _6)
-
-  ffmpeg_4 = self.ffmpeg_4-rpi; # small
-  ffmpeg_4-headless = self.ffmpeg_4-rpi.override {
-    ffmpegVariant = "headless";
-  };
-  ffmpeg_4-full = self.ffmpeg_4-rpi.override {
-    ffmpegVariant = "full";
-  };
-
-  ffmpeg_5 = self.ffmpeg_5-rpi; # small
-  ffmpeg_5-headless = self.ffmpeg_5-rpi.override {
-    ffmpegVariant = "headless";
-  };
-  ffmpeg_5-full = self.ffmpeg_5-rpi.override {
-    ffmpegVariant = "full";
-  };
-  
-  ffmpeg_6 = self.ffmpeg_6-rpi; # small
-  ffmpeg_6-headless = self.ffmpeg_6-rpi.override {
-    ffmpegVariant = "headless";
-  };
-  ffmpeg_6-full = self.ffmpeg_6-rpi.override {
-    ffmpegVariant = "full";
-  };
 
   ffmpeg_4-rpi = (super.callPackage ../../pkgs/ffmpeg_4-rpi.nix {
     ffmpeg = super.ffmpeg_4;
@@ -83,14 +56,6 @@ self: super: { # final: prev:
   ffmpeg_6-rpi = (super.callPackage ../../pkgs/ffmpeg_6-rpi.nix {
     ffmpeg = super.ffmpeg_6;
   });
-
-
-  # as in `...ffmpeg/default.nix`
-  # need to override those because as well, because there
-  # those definitions are recursively local
-  ffmpeg = self.ffmpeg_6; # chromaprint
-  ffmpeg-headless = self.ffmpeg_6-headless; # python3.11-matplotlib
-  ffmpeg-full = self.ffmpeg_6-full;
 
 
   # an attempt for `nixpkgs` with RPi4-specific compiler optimizations
