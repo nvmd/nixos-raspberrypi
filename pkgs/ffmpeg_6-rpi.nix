9467c89ea0f46a39640e648c94b9de37d6115336
Sergey Kazenyuk <kazenyuk@pm.me>
2024-06-01 02:08:12 +0300

    ffmpeg-rpi: use 6.0.1 branch, maybe its more stable

--- pkgs/ffmpeg_6-rpi.nix
+++ pkgs/ffmpeg_6-rpi.nix
@@ -6,22 +6,23 @@
 
 let
   # https://github.com/jc-kynesim/rpi-ffmpeg/commits/dev/6.1.1/rpi_import_1/
-  ffmpegVersion = "6.1.1";
-  rpiFfmpegSrc = fetchFromGitHub {
-    owner = "jc-kynesim";
-    repo  = "rpi-ffmpeg";
-    rev   = "dev/${ffmpegVersion}/rpi_import_1";
-    hash  = "sha256-A5+aGmYijEH+GPRCHpGpK742Bvu/cT6R2ZezIE8S6h4=";
-    # hash  = lib.fakeHash;
-  };
-  # https://github.com/jc-kynesim/rpi-ffmpeg/commits/test/6.0.1/main
-  # ffmpegVersion = "6.0.1";
+  # ffmpegVersion = "6.1.1";
   # rpiFfmpegSrc = fetchFromGitHub {
   #   owner = "jc-kynesim";
   #   repo  = "rpi-ffmpeg";
-  #   rev   = "test/${ffmpegVersion}/main";
-  #   hash  = "sha256-DvtFmWQcXB/5l3g52uJGBU9GtzRgzF/RDJP3LR5yU5c=";
+  #   rev   = "dev/${ffmpegVersion}/rpi_import_1";
+  #   hash  = "sha256-A5+aGmYijEH+GPRCHpGpK742Bvu/cT6R2ZezIE8S6h4=";
+  #   # hash  = lib.fakeHash;
   # };
+  # https://github.com/jc-kynesim/rpi-ffmpeg/commits/test/6.0.1/main
+  ffmpegVersion = "6.0.1";
+  rpiFfmpegSrc = fetchFromGitHub {
+    owner = "jc-kynesim";
+    repo  = "rpi-ffmpeg";
+    rev   = "test/${ffmpegVersion}/main";
+    hash  = "sha256-9wqA1OU0/otiQGlUGAupIP/DSDlf0CLqhN8CQ2WCvbk=";
+    # hash  = lib.fakeHash;
+  };
   # see also for configure flags
   # https://github.com/jc-kynesim/rpi-ffmpeg/blob/test/6.0.1/main/pi-util/conf_native.sh#L110
 
