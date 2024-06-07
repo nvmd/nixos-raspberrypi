cac068bf1bd38e23889c1aedbe38f4edab28298b
Sergey Kazenyuk <kazenyuk@pm.me>
2024-06-08 02:18:41 +0300

    raspberrypi: add latest (but deprecated) libraspberrypi

--- overlays/raspberrypi/vendor-utils.nix
+++ overlays/raspberrypi/vendor-utils.nix
@@ -1,5 +1,7 @@
 self: super: { # final: prev:
 
+  libraspberrypi = super.callPackage ../../pkgs/libraspberrypi.nix {};
+
   raspberrypi-utils = super.callPackage ../../pkgs/raspberrypi-utils.nix {};
 
   libpisp = self.callPackage ../../pkgs/raspberrypi/libpisp.nix {};
---
+++ pkgs/libraspberrypi.nix
@@ -0,0 +1,33 @@
+{ lib, stdenv
+, fetchFromGitHub
+, cmake
+, pkg-config
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libraspberrypi";
+  version = "unstable-2023-10-23";
+
+  src = fetchFromGitHub {
+    owner = "raspberrypi";
+    repo = "userland";
+    rev = "96a7334ae9d5fc9db7ac92e59852377df63f1848";
+    hash = lib.fakeHash;
+  };
+
+  nativeBuildInputs = [ cmake pkg-config ];
+  cmakeFlags = [
+    # -DARM64=ON disables all targets that only build on 32-bit ARM; this allows
+    # the package to build on aarch64 and other architectures
+    "-DARM64=${if stdenv.hostPlatform.isAarch32 then "OFF" else "ON"}"
+    "-DVMCS_INSTALL_PREFIX=${placeholder "out"}"
+  ];
+
+  meta = with lib; {
+    description = "ARM side libraries for interfacing to Raspberry Pi GPU";
+    homepage = "https://github.com/raspberrypi/userland";
+    license = licenses.bsd3;
+    platforms = [ "armv6l-linux" "armv7l-linux" "aarch64-linux" "x86_64-linux" ];
+    maintainers = with maintainers; [ dezgeg tkerber ];
+  };
+})
