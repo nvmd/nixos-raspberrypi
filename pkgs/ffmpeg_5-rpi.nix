52ef8f2cc6f3e117d8b940492d2e25196c51e4ef
Sergey Kazenyuk <kazenyuk@pm.me>
2024-07-01 00:41:25 +0300

    ffmpeg_5-rpi: latest 5.1

--- pkgs/ffmpeg_5-rpi.nix
+++ pkgs/ffmpeg_5-rpi.nix
@@ -7,13 +7,13 @@
 let
   # there are, in the order of latest changes:
   # * https://github.com/jc-kynesim/rpi-ffmpeg/commits/release/5.1/main
-  # * https://github.com/jc-kynesim/rpi-ffmpeg/commits/pi/5.1.4/rpi_23
   # * https://github.com/jc-kynesim/rpi-ffmpeg/commits/test/5.1.4/main
-  ffmpegVersion = "5.1.4";
+  # * https://github.com/jc-kynesim/rpi-ffmpeg/commits/pi/5.1.4/rpi_23
+  ffmpegVersion = "5.1";
   rpiFfmpegSrc = fetchFromGitHub {
     owner = "jc-kynesim";
     repo  = "rpi-ffmpeg";
-    rev   = "test/${ffmpegVersion}/main";
+    rev   = "release/${ffmpegVersion}/main";
     # hash  = "sha256-lTcX0C5PdKWch4nB+TCH51IIjvbSrsyyurGonYR8EFU=";
     hash  = lib.fakeHash;
   };
