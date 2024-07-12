a0811a09c4c51da87921132b7217193ea7494dd2
Sergey Kazenyuk <kazenyuk@pm.me>
2024-07-13 01:31:22 +0300

    lib: raspberrypi overlays' injection

--- lib/raspberrypi.nix
+++ lib/raspberrypi.nix
@@ -7,4 +7,30 @@
       inherit variant bootloader;
     };
   });
+
+  inject-raspberrypi-overlays = rpi-nixpkgs-base: { lib, ... }: {
+    nixpkgs.overlays = lib.mkBefore [
+      (self: super: { # final: prev:
+        rpi = import rpi-nixpkgs-base {
+          inherit (self) system config;
+          overlays = [
+            (import ../overlays/raspberrypi/vendor-utils.nix)
+            (import ../overlays/raspberrypi/pkgs.nix)
+            (import ../overlays/raspberrypi/pkgs-global.nix)
+          ];
+        };
+      })
+      (import ../overlays/raspberrypi/pkgs.nix)
+      (import ../overlays/raspberrypi/vendor-utils.nix)
+      (import ../overlays/raspberrypi/vendor-kernel.nix)
+      # (import ./overlays/raspberrypi/vendor-kernel-nixpkgs.nix)
+      (import ../overlays/raspberrypi/bootloader.nix)
+    ];
+  };
+  inject-raspberrypi-overlays-global = { lib, ... }: {
+    nixpkgs.overlays = [
+      # !!! causes a _lots_ of rebuilds for graphical stuff via ffmpeg, SDL2
+      (import ../overlays/raspberrypi/pkgs-global.nix)
+    ];
+  };
 }
