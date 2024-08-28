b97dafcac53763de07b9524203d28113ae6eceac
Sergey Kazenyuk <kazenyuk@pm.me>
2024-08-28 16:23:50 +0300

    rpicam-apps: enable qt preview

--- pkgs/raspberrypi/rpicam-apps.nix
+++ pkgs/raspberrypi/rpicam-apps.nix
@@ -1,9 +1,9 @@
 { lib
 , stdenv
 , fetchFromGitHub
-, pkg-config
 , meson
 , ninja
+, pkg-config
 , boost
 , ffmpeg
 , libcamera
@@ -12,11 +12,13 @@
 , libjpeg
 , libpng
 , libtiff
-, withDrmPreview ? true #default=true
-, withEglPreview ? true #default=true
+, withDrmPreview ? true
+, withEglPreview ? true
 , libepoxy
 , libGL
 , libX11
+, withQtPreview ? true
+, qt5
 , withOpenCVPostProc ? true  #default=false
 , opencv
 }:
@@ -33,22 +35,28 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-s4zJh6r3VhiquO54KWZ78dVCH1BmlphY9zEB9BidNyo=";
   };
 
-  nativeBuildInputs = [ pkg-config meson ninja ];
+  nativeBuildInputs = [
+    meson ninja pkg-config
+  ] ++ lib.optional withQtPreview qt5.wrapQtAppsHook;
+
   buildInputs = [
     boost ffmpeg libexif libcamera
     libdrm  # needed even with drm preview disabled
     libjpeg libpng libtiff
-  ] ++ lib.optionals withEglPreview [ libepoxy libX11 libGL ]
+  ] ++ lib.optionals withQtPreview (with qt5; [ qtbase qttools ])
+    ++ lib.optionals withEglPreview [ libepoxy libX11 libGL ]
     ++ lib.optionals withOpenCVPostProc [ opencv ];
+
   mesonFlags = [
     # preview
-    "${lib.mesonEnable "enable_drm" withDrmPreview}"
-    "${lib.mesonEnable "enable_egl" withEglPreview}"
-    "-Denable_qt=disabled"  #default=enabled
+    (lib.mesonEnable "enable_drm" withDrmPreview)
+    (lib.mesonEnable "enable_egl" withEglPreview)
+    (lib.mesonEnable "enable_qt" withQtPreview)
     # postprocessing
-    "${lib.mesonEnable "enable_opencv" withOpenCVPostProc}"
+    (lib.mesonEnable "enable_opencv" withOpenCVPostProc)
     "-Denable_hailo=disabled" #default=enabled, hailort (HailoRT)
   ];
+
   # Meson is no longer able to pick up Boost automatically.
   # https://github.com/NixOS/nixpkgs/issues/86131
   BOOST_INCLUDEDIR = "${lib.getDev boost}/include";
