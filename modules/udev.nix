e500a3884c0c2fb25286e4dd351dbc758d40ecde
Sergey Kazenyuk <kazenyuk@pm.me>
2024-09-23 03:33:59 +0300

    udev: get rules from the package, not extraRules

--- modules/udev.nix
+++ modules/udev.nix
@@ -2,6 +2,7 @@
 
 {
   # ensure these groups used by udev rules exist
+  # as of raspberrypi-udev-rules-20240911
   users.extraGroups = {
     gpio = {};
     i2c = {};
@@ -11,86 +12,7 @@
     video = {};
   };
 
-  services.udev.extraRules = let
-    sh = "${pkgs.bash}/bin/sh";  # /bin/sh
-    grep = "${lib.getExe pkgs.gnugrep}"; # /bin/grep
-    chgrp = "chgrp";  # /bin/chgrp
-    chmod = "chmod";  # /bin/chmod
-    test = "${pkgs.coreutils}/bin/test";  # /usr/bin/test
-  in ''
-    # https://github.com/RPi-Distro/raspberrypi-sys-mods/blob/bookworm/lib/udev/rules.d/10-vc.rules
-    KERNEL=="vcio", GROUP="video", MODE="0660"
-    KERNEL=="vchiq", GROUP="video", MODE="0660"
-    SUBSYSTEM=="vc-sm", GROUP="video", MODE="0660"
-    KERNEL=="vcsm-cma", GROUP="video", MODE="0660"
-
-
-    # https://github.com/RPi-Distro/raspberrypi-sys-mods/blob/bookworm/lib/udev/rules.d/15-i2c-modprobe.rules
-    # https://github.com/RPi-Distro/raspberrypi-sys-mods/commit/5582ff9ed61c429cf700a0beca2a2af3afcbab04
-    # SUBSYSTEM=="i2c|spi", ENV{MODALIAS}=="?*", ENV{OF_NAME}=="?*", ENV{OF_COMPATIBLE_0}=="?*", RUN+="/usr/lib/raspberrypi-sys-mods/i2cprobe"
-
-
-    # https://github.com/RPi-Distro/raspberrypi-sys-mods/blob/bookworm/lib/udev/rules.d/60-backlight.rules
-    # SUBSYSTEM=="backlight", ACTION=="add", RUN+="${chgrp} video $sys$devpath/brightness", RUN+="${chmod} g+w $sys$devpath/brightness"
-
-
-    # https://github.com/RPi-Distro/raspberrypi-sys-mods/blob/bookworm/lib/udev/rules.d/60-dma-heap.rules
-    SUBSYSTEM=="dma_heap", GROUP="video", MODE="0660"
-    SUBSYSTEM=="dma_heap", KERNEL=="system", PROGRAM="${grep} -q \"^Revision\s*:\s*[ 123][0-9a-fA-F][0-9a-fA-F]4[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]$$\" /proc/cpuinfo", SYMLINK+="dma_heap/vidbuf_cached"
-    SUBSYSTEM=="dma_heap", KERNEL=="linux,cma", SYMLINK+="dma_heap/vidbuf_cached", OPTIONS+="link_priority=-50"
-
-
-    # https://github.com/RPi-Distro/raspberrypi-sys-mods/blob/bookworm/lib/udev/rules.d/60-gpiochip4.rules
-    SUBSYSTEM=="gpio", KERNEL=="gpiochip0", PROGRAM="${test} ! -c /dev/gpiochip4", SYMLINK+="gpiochip4"
-
-
-    # https://github.com/RPi-Distro/raspberrypi-sys-mods/blob/bookworm/lib/udev/rules.d/60-pico.rules
-    ATTRS{manufacturer}=="Raspberry Pi", MODE="660", GROUP="plugdev", TAG+="uaccess"
-
-
-    # https://github.com/RPi-Distro/raspberrypi-sys-mods/blob/bookworm/lib/udev/rules.d/70-microbit.rules
-    SUBSYSTEM=="usb", ATTR{idVendor}=="0d28", TAG+="uaccess"
-
-
-    # https://github.com/RPi-Distro/raspberrypi-sys-mods/blob/bookworm/etc.armhf/udev/rules.d/99-com.rules
-    SUBSYSTEM=="input", GROUP="input", MODE="0660"
-    SUBSYSTEM=="i2c-dev", GROUP="i2c", MODE="0660"
-    SUBSYSTEM=="spidev", GROUP="spi", MODE="0660"
-    SUBSYSTEM=="*gpiomem*", GROUP="gpio", MODE="0660"
-    SUBSYSTEM=="rpivid-*", GROUP="video", MODE="0660"
-
-    SUBSYSTEM=="gpio", GROUP="gpio", MODE="0660"
-    SUBSYSTEM=="gpio", KERNEL=="gpiochip*", ACTION=="add", PROGRAM="${sh} -c 'chgrp -R gpio /sys/class/gpio && chmod -R g=u /sys/class/gpio'"
-    SUBSYSTEM=="gpio", ACTION=="add", PROGRAM="${sh} -c 'chgrp -R gpio /sys%p && chmod -R g=u /sys%p'"
-
-    # PWM export results in a "change" action on the pwmchip device (not "add" of a new device), so match actions other than "remove".
-    SUBSYSTEM=="pwm", ACTION!="remove", PROGRAM="${sh} -c 'chgrp -R gpio /sys%p && chmod -R g=u /sys%p'"
-
-    KERNEL=="ttyAMA[0-9]*|ttyS[0-9]*", PROGRAM="${sh} -c '\
-            ALIASES=/proc/device-tree/aliases; \
-            TTYNODE=$$(readlink /sys/class/tty/%k/device/of_node | sed 's/base/:/' | cut -d: -f2); \
-            if [ -e $$ALIASES/bluetooth ] && [ $$TTYNODE/bluetooth = $$(strings $$ALIASES/bluetooth) ]; then \
-                echo 1; \
-            elif [ -e $$ALIASES/console ]; then \
-                if [ $$TTYNODE = $$(strings $$ALIASES/console) ]; then \
-                    echo 0;\
-                else \
-                    exit 1; \
-                fi \
-            elif [ $$TTYNODE = $$(strings $$ALIASES/serial0) ]; then \
-                echo 0; \
-            elif [ $$TTYNODE = $$(strings $$ALIASES/serial1) ]; then \
-                echo 1; \
-            else \
-                exit 1; \
-            fi \
-    '", SYMLINK+="serial%c"
-
-    ACTION=="add", SUBSYSTEM=="vtconsole", KERNEL=="vtcon1", RUN+="${sh} -c '\
-      if echo RPi-Sense FB | cmp -s /sys/class/graphics/fb0/name; then \
-        echo 0 > /sys$devpath/bind; \
-      fi; \
-    '"
-
-  '';
+  services.udev.packages = [
+    (pkgs.callPackage ../pkgs/raspberrypi/udev-rules.nix {})
+  ];
 }
---
+++ pkgs/raspberrypi/udev-rules.nix
@@ -0,0 +1,72 @@
+{ lib
+, stdenvNoCC
+, fetchFromGitHub
+, bash
+, gnugrep
+, coreutils
+}:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "raspberrypi-udev-rules";
+  version = "20240911";
+
+  # https://github.com/RPi-Distro/raspberrypi-sys-mods/tree/bookworm/
+  src = fetchFromGitHub {
+    owner = "RPi-Distro";
+    repo = "raspberrypi-sys-mods";
+    rev = "22b66366ef9bdcdc83de5be883931b7af9c55904";
+    hash = "sha256-an8oS62KJZyyU6jTnnoGW5WOm3UD4JZ/leR4jyeIlU8=";
+  };
+
+  installPhase = ''
+    mkdir -p $out/etc/udev/rules.d
+    mkdir -p $out/lib/udev/rules.d
+
+    # Install only explicitly listed rules
+
+    rules_etc_src=etc.armhf/udev/rules.d
+    declare -a rules_etc=(
+      99-com.rules
+    )
+
+    rules_lib_src=lib/udev/rules.d
+    declare -a rules_lib=(
+      10-vc.rules
+      15-i2c-modprobe.rules
+      60-backlight.rules
+      60-dma-heap.rules
+      60-gpiochip4.rules
+      60-pico.rules
+      70-microbit.rules
+      80-noobs.rules
+    )
+
+    for i in "''${rules_lib[@]}"; do
+      install -vD "$rules_lib_src/$i" $out/lib/udev/rules.d
+    done
+    for i in "''${rules_etc[@]}"; do
+      install -vD "$rules_etc_src/$i" $out/etc/udev/rules.d
+    done
+  '';
+
+  fixupPhase = ''
+    for i in $out/{etc,lib}/udev/rules.d/*.rules; do
+      substituteInPlace $i \
+        --replace-quiet \"/bin/sh \"${bash}/bin/sh \
+        --replace-quiet \"/bin/grep \"${lib.getExe gnugrep} \
+        --replace-quiet \"/bin/chgrp \"chgrp \
+        --replace-quiet \"/bin/chmod \"chmod \
+        --replace-quiet /usr/bin/test ${coreutils}/bin/test
+    done
+  '';
+
+  meta = with lib; {
+    description = "A collection of Raspberry Pi-sourced system configuration files and associated scripts";
+    homepage = "https://github.com/RPi-Distro/raspberrypi-sys-mods/";
+    # https://github.com/RPi-Distro/raspberrypi-sys-mods/blob/master/debian/copyright
+    license = licenses.bsd3;
+    platforms = platforms.all;
+    # buildable by all, but will make sense only on these, obviously
+    # platforms = [ "aarch64-linux" "armv7l-linux" "armv6l-linux" ];
+  };
+})
