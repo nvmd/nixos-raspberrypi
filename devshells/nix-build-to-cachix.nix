32a9c4f428bf344d00f15cd1df8bb9280c75e54a
Sergey Kazenyuk <kazenyuk@pm.me>
2024-09-24 12:04:36 +0300

    nix-build-to-cachix: simplify, cleanup

--- devshells/nix-build-to-cachix.nix
+++ devshells/nix-build-to-cachix.nix
@@ -11,24 +11,15 @@ writeShellApplication {
 
 
     build_and_push() {
-      local prefix="$1"
-      local target="$2"
+      local target="$1"
 
-      nix build "''${prefix}.''${target}" --json \
+      nix build "''${target}" --json \
         | jq -r '.[].outputs | to_entries[].value' \
         | cachix push "''${CACHE}"
     }
 
     build_and_push_packages() {
-      build_and_push ".#packages.aarch64-linux" "$1"
-    }
-
-    build_and_push_nixpkgs() {
-      build_and_push ".#legacyPackages.aarch64-linux" "$1"
-    }
-
-    build_and_push_nixpkgs_unstable() {
-      build_and_push ".#legacyPackagesUnstable.aarch64-linux" "$1"
+      build_and_push ".#packages.aarch64-linux.$1"
     }
 
 
@@ -50,55 +41,33 @@ writeShellApplication {
     declare -a packages=(
       "ffmpeg_4"
       "ffmpeg_6"
+      # "ffmpeg_7"
 
       # "kodi"
       "kodi-gbm"
-      "kodi-wayland"
+      # "kodi-wayland"
 
       "libcamera"
       "libpisp"
       "libraspberrypi"
 
+      "raspberrypi-utils"
+      "raspberrypi-udev-rules"
+      "rpicam-apps"
+
       "SDL2"
 
       "vlc"
 
+      # linuxAndFirmware.latest.*
       "linux_rpi4"
       "linux_rpi5"
       "raspberrypifw"
       "raspberrypiWirelessFirmware"
     )
-    declare -a nixpkgs=(
-      "libcamera"
-      "libpisp"
-      "libraspberrypi"
-
-      "SDL2"
-
-      "vlc"
-
-      "linuxAndFirmware.latest.linux_rpi4"
-      "linuxAndFirmware.latest.linux_rpi5"
-      "linuxAndFirmware.latest.raspberrypifw"
-      "linuxAndFirmware.latest.raspberrypiWirelessFirmware"
-    )
-    declare -a nixpkgs_unstable=(
-      "linuxAndFirmware.latest.linux_rpi4"
-      # "linuxAndFirmware.latest.linux_rpi5"
-      "linuxAndFirmware.latest.raspberrypifw"
-      "linuxAndFirmware.latest.raspberrypiWirelessFirmware"
-    )
 
     for i in "''${packages[@]}"; do
       build_and_push_packages "$i"
-    done
-
-    for i in "''${nixpkgs[@]}"; do
-      build_and_push_nixpkgs "$i"
-    done
-
-    for i in "''${nixpkgs_unstable[@]}"; do
-      build_and_push_nixpkgs_unstable "$i"
     done
 
     set +o xtrace
