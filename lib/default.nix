4c9dbbf575246ac8da800e9e9ec343347bf2ed74
Sergey Kazenyuk <kazenyuk@pm.me>
2025-03-09 02:23:50 +0300

    overlays: propagate config.allowUnfree* for rpi "channel"

--- lib/default.nix
+++ lib/default.nix
@@ -4,7 +4,25 @@
   inject-overlays = { config, lib, ... }: {
     nixpkgs.overlays = [
       (final: prev: {
-        rpi = self.legacyPackages.${config.nixpkgs.system};
+        rpi = import self.inputs.nixpkgs {
+          inherit (prev) system;
+          config = {
+            inherit (prev.config) allowUnfree allowUnfreePredicate;
+          };
+
+          overlays = [
+            self.overlays.bootloader
+
+            self.overlays.pkgs
+
+            self.overlays.vendor-pkgs
+
+            self.overlays.vendor-firmware
+            self.overlays.vendor-kernel
+
+            self.overlays.kernel-and-firmware
+          ];
+        };
       })
 
       self.overlays.bootloader
