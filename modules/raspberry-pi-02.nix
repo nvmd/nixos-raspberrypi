eb9acc2ec42a1fb25a022673cd79b1ea5adb2b6f
Sergey Kazenyuk <kazenyuk@pm.me>
2025-03-24 06:51:37 +0300

    pi-02: workaround for "modprobe: FATAL: Module <module name> not found"
    See https://github.com/NixOS/nixpkgs/issues/154163

--- modules/raspberry-pi-02.nix
+++ modules/raspberry-pi-02.nix
@@ -10,4 +10,14 @@
   };
 
   boot.kernelPackages = lib.mkDefault self.packages.${pkgs.hostPlatform.system}.linuxPackages_rpi02;
+
+  # workaround for "modprobe: FATAL: Module <module name> not found"
+  # see https://github.com/NixOS/nixpkgs/issues/154163,
+  #     https://github.com/NixOS/nixpkgs/issues/154163#issuecomment-1350599022
+  nixpkgs.overlays = [
+    (final: super: {
+      makeModulesClosure = x:
+        super.makeModulesClosure (x // { allowMissing = true; });
+    })
+  ];
 }
