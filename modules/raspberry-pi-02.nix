c89e1ec6bdbfc8602d9cbc0a9a3450d83a938291
Sergey Kazenyuk <kazenyuk@pm.me>
2025-04-14 00:49:52 +0300

    rpi5,4,02: workaround for "modprobe: FATAL: Module <module name> not found
    move to the common module

--- modules/raspberry-pi-02.nix
+++ modules/raspberry-pi-02.nix
@@ -10,14 +10,4 @@
   };
 
   boot.kernelPackages = lib.mkDefault self.packages.${pkgs.hostPlatform.system}.linuxPackages_rpi02;
-
-  # workaround for "modprobe: FATAL: Module <module name> not found"
-  # see https://github.com/NixOS/nixpkgs/issues/154163,
-  #     https://github.com/NixOS/nixpkgs/issues/154163#issuecomment-1350599022
-  nixpkgs.overlays = [
-    (final: super: {
-      makeModulesClosure = x:
-        super.makeModulesClosure (x // { allowMissing = true; });
-    })
-  ];
 }
--- modules/raspberrypi.nix
+++ modules/raspberrypi.nix
@@ -47,4 +47,14 @@
   environment.systemPackages = with pkgs; [
     raspberrypi-utils
   ];
+
+  # workaround for "modprobe: FATAL: Module <module name> not found"
+  # see https://github.com/NixOS/nixpkgs/issues/154163,
+  #     https://github.com/NixOS/nixpkgs/issues/154163#issuecomment-1350599022
+  nixpkgs.overlays = [
+    (final: super: {
+      makeModulesClosure = x:
+        super.makeModulesClosure (x // { allowMissing = true; });
+    })
+  ];
 }
