86948b63658c7c6b04b6e705f90a09ca6b877771
Sergey Kazenyuk <kazenyuk@pm.me>
2025-05-21 01:42:14 +0300

    ffmpeg_7: 2025-03-10

--- pkgs/ffmpeg_7-rpi.nix
+++ pkgs/ffmpeg_7-rpi.nix
@@ -6,13 +6,13 @@
 
 let
   # https://github.com/jc-kynesim/rpi-ffmpeg/tree/test/7.1/main
-  ffmpegVersion = "7.1";
+  ffmpegVersion = "7.1.1";
   rpiFfmpegSrc = fetchFromGitHub {
     owner = "jc-kynesim";
     repo  = "rpi-ffmpeg";
     # rev   = "test/${ffmpegVersion}/main"; # this branch is being forced-push to
-    rev = "024838d04594c4acff3f8bc79fe885482dea8121";
-    hash  = "sha256-Lt/4eeW1f84ysCNp/5HG7qTLNnyEuKuMUBY/PW+REqc=";
+    rev = "6dbf87aefd7f491210abe1e043a1c228fa1439a0";
+    hash  = "sha256-cLDfw1lyZTny39OfW+dvZaTkZz3zkNdEpvPh1dgHJ2I=";
   };
 
 in callPackage ./ffmpeg-rpi.nix {
