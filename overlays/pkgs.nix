15dc31f8c3c68d5ee3ed4e47a70a6d5d180934d3
Sergey Kazenyuk <kazenyuk@pm.me>
2025-05-21 02:13:28 +0300

    pkgs: cleanup libcamera

--- overlays/pkgs.nix
+++ overlays/pkgs.nix
@@ -94,17 +94,6 @@ self: super: { # final: prev:
       hash = "sha256-7nXoYgwV354T42QM9LA9U0xlG2f4Gc7I1inaEG/HwTI=";
     };
 
-    # not needed for nixpkgs-unstable
-    postPatch = ''
-      ${old.postPatch}
-      patchShebangs src/py/
-    '';
-
-    buildInputs = old.buildInputs ++ (with self; [
-      libpisp
-      python3Packages.pybind11  # not needed for nixpkgs-unstable
-    ]);
-
     mesonFlags = old.mesonFlags ++ [
       # add flags that raspberry suggests, but nixpkgs doesn't include
       "--buildtype=release"
