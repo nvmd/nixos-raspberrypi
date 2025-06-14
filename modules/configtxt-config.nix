e62268c8931b653a5fc283116a32ff6b3db3979d
Sergey Kazenyuk <kazenyuk@pm.me>
2025-06-14 20:44:53 +0300

    configtxt-config: improve readability

--- modules/configtxt-config.nix
+++ modules/configtxt-config.nix
@@ -7,50 +7,48 @@
 { lib, config, pkgs, ... }:
 let
   cfg = config.hardware.raspberry-pi;
-  render-raspberrypi-config =
-    let
-      render-options = opts:
-        lib.strings.concatStringsSep "\n" (render-dt-kvs opts);
-      render-dt-param = x: "dtparam=" + x;
-      render-dt-kv = k: v:
-        if isNull v.value then
-          k
-        else
-          let vstr = toString v.value; in "${k}=${vstr}";
-      render-dt-kvs = x:
-        lib.attrsets.mapAttrsToList render-dt-kv
-          (lib.filterAttrs (k: v: v.enable) x);
-      render-dt-overlay = { overlay, args }:
-        "dtoverlay=" + overlay + "\n"
-        + lib.strings.concatMapStringsSep "\n" render-dt-param args + "\n"
-        + "dtoverlay=";
-      render-base-dt-params = params:
-        lib.strings.concatMapStringsSep "\n" render-dt-param
-          (render-dt-kvs params);
-      render-dt-overlays = overlays:
-        lib.strings.concatMapStringsSep "\n" render-dt-overlay
-          (lib.attrsets.mapAttrsToList
-            (k: v: {
-              overlay = k;
-              args = render-dt-kvs v.params;
-            })
-            (lib.filterAttrs (k: v: v.enable) overlays));
-      render-config-section = k:
-        { options, base-dt-params, dt-overlays }:
-        let
-          all-config = lib.concatStringsSep "\n" (lib.filter (x: x != "") [
-            (render-options options)
-            (render-base-dt-params base-dt-params)
-            (render-dt-overlays dt-overlays)
-          ]);
-        in
-        ''
-          [${k}]
-          ${all-config}
-        '';
-    in
-    conf:
-    lib.strings.concatStringsSep "\n"
+
+  render-raspberrypi-config = let
+
+    render-options = opts:
+      lib.strings.concatStringsSep "\n" (render-dt-kvs opts);
+    render-dt-param = x: "dtparam=" + x;
+    render-dt-kv = k: v:
+      if isNull v.value then k
+      else "${k}=${toString v.value}";
+
+    render-dt-kvs = x:
+      lib.attrsets.mapAttrsToList render-dt-kv
+        (lib.filterAttrs (k: v: v.enable) x);
+
+    render-dt-overlay = { overlay, args }:
+      "dtoverlay=" + overlay + "\n"
+      + lib.strings.concatMapStringsSep "\n" render-dt-param args + "\n"
+      + "dtoverlay=";
+    render-base-dt-params = params:
+      lib.strings.concatMapStringsSep "\n" render-dt-param
+        (render-dt-kvs params);
+    render-dt-overlays = overlays:
+      lib.strings.concatMapStringsSep "\n" render-dt-overlay
+        (lib.attrsets.mapAttrsToList
+          (k: v: {
+            overlay = k;
+            args = render-dt-kvs v.params;
+          })
+          (lib.filterAttrs (k: v: v.enable) overlays));
+
+    render-config-section = k:
+      { options, base-dt-params, dt-overlays }:
+      let all-config = lib.concatStringsSep "\n" (lib.filter (x: x != "") [
+          (render-options options)
+          (render-base-dt-params base-dt-params)
+          (render-dt-overlays dt-overlays)
+        ]);
+      in ''
+        [${k}]
+        ${all-config}
+      '';
+  in conf: lib.strings.concatStringsSep "\n"
       ((lib.attrsets.mapAttrsToList render-config-section conf)
       ++ [ cfg.extra-config ]);
 in {
