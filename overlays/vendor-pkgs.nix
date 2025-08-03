fbfe16ae98c144f7bcc351eda15e8a790d7cb04c
adminy <22717869+adminy@users.noreply.github.com>
2025-08-03 08:05:39 +0100

    libpisp: overlay overwrite json backend

--- overlays/vendor-pkgs.nix
+++ overlays/vendor-pkgs.nix
@@ -1,7 +1,21 @@
 self: super: { # final: prev:
 
-  # now available from nixpkgs
-  # libpisp = self.callPackage ../pkgs/raspberrypi/libpisp.nix {};
+  # now available from nixpkgs but waiting on https://github.com/NixOS/nixpkgs/pull/429288
+  libpisp = super.libpisp.overrideAttrs (old: {
+    preFixup = ''
+      so=$out/lib/libpisp.so.1
+      patchelf --set-soname $so $so
+      patchelf --remove-rpath $so
+      needed=$(patchelf --print-needed $so)
+      for pkg in ${super.glibc} ${super.boost} ${super.libgcc} ${super.stdenv.cc.cc.lib}; do
+        for lib in $needed; do
+          for libpath in $(find -L $pkg/lib -type f -name "$lib"); do
+            patchelf --replace-needed $lib $libpath $so
+          done
+        done
+      done
+    '';
+  });
 
   libraspberrypi = super.callPackage ../pkgs/raspberrypi/libraspberrypi.nix {};
 
