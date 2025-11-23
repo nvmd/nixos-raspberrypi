fbb98819de8edb066bc8f86faf52f60f7b28885f
robert jakub <rj@project2.pl>
2025-11-23 10:54:52 +0100

    'hostPlatform' has been renamed to/replaced by 'stdenv.hostPlatform'

--- modules/raspberry-pi-02.nix
+++ modules/raspberry-pi-02.nix
@@ -1,4 +1,4 @@
-{ self, config, lib, pkgs, ... }:
+{ self, lib, pkgs, ... }:
 
 {
   imports = [ ./raspberrypi.nix ];
@@ -6,8 +6,8 @@
   boot.loader.raspberryPi = {
     variant = "02";
     bootloader = lib.mkDefault "uboot";
-    firmwarePackage = lib.mkDefault self.packages.${pkgs.hostPlatform.system}.raspberrypifw;
+    firmwarePackage = lib.mkDefault self.packages.${pkgs.stdenv.hostPlatform.system}.raspberrypifw;
   };
 
-  boot.kernelPackages = lib.mkDefault self.packages.${pkgs.hostPlatform.system}.linuxPackages_rpi02;
+  boot.kernelPackages = lib.mkDefault self.packages.${pkgs.stdenv.hostPlatform.system}.linuxPackages_rpi02;
 }
