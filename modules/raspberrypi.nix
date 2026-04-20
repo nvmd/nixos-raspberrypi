24d2a35e374df4306fea2f5f9c4ac2ec27aaed72
Lily Blanleuil <lily.blanleuil@naostage.com>
2026-04-20 10:33:41 +0200

    🐛 make uart non-tty by default

--- modules/raspberrypi.nix
+++ modules/raspberrypi.nix
@@ -31,7 +31,7 @@
   boot.consoleLogLevel = lib.mkDefault 7;
   # https://github.com/raspberrypi/firmware/issues/1539#issuecomment-784498108
   # https://github.com/RPi-Distro/pi-gen/blob/master/stage1/00-boot-files/files/cmdline.txt
-  boot.kernelParams = [ "console=serial0,115200n8" "console=tty1" ];
+  boot.kernelParams = [ "console=tty1" ];
 
   boot.initrd.availableKernelModules = [
     "xhci_pci"
