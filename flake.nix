19f53ada5ede32d171956f06a842e72016010931
Sergey Kazenyuk <git.v63hf@sergey-kazenyuk.com>
2026-06-20 22:24:03 +0000

    flake: nixos-unstable

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     argononed = {
       # url = "git+file:../argononed?shallow=1";
